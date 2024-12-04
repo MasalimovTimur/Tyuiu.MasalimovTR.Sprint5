82b2b925296d7807fa7fc817b5822ad6571f0ff9
MasalimovTimur <masalimovtr@gmail.com>
2024-12-04 17:56:53 +0500

    Task2

--- Tyuiu.MasalimovTR.Sprint5.Task2.V14.Lib/DataService.cs
+++ Tyuiu.MasalimovTR.Sprint5.Task2.V14.Lib/DataService.cs
@@ -1,7 +1,16 @@
-﻿namespace Tyuiu.MasalimovTR.Sprint5.Task2.V14.Lib
-{
-    public class DataService
-    {
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
 
+namespace Tyuiu.MasalimovTR.Sprint5.Task2.V14.Lib
+{
+    public class DataService : ISprint5Task2V14
+    {
+        public string SaveToFileTextData(int[,] matrix)
+        {
+            string path = Path.Combine(new string[] { Path.GetTempPath(), "OutPutFileTask2.txt" });
+            string y = "11";
+            
+            File.WriteAllText(path, y);
+            return path;
+        }
     }
 }
