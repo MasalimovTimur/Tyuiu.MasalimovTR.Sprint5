4f88ad1bd4a81ea7234ea9de929e6d772805a668
MasalimovTimur <masalimovtr@gmail.com>
2024-12-04 17:28:09 +0500

    Task1

--- Tyuiu.MasalimovTR.Sprint5.Task1.V1.Lib/DataService.cs
+++ Tyuiu.MasalimovTR.Sprint5.Task1.V1.Lib/DataService.cs
@@ -8,7 +8,8 @@ namespace Tyuiu.MasalimovTR.Sprint5.Task1.V1.Lib
         {
             string path = Path.Combine(new string[] { Path.GetTempPath(), "OutPutFileTask1.txt" });
             double y = Math.Round((3 * Math.Pow(startValue, 4) + 1) / Math.Pow(stopValue, 3), 3);
-            File.WriteAllText(path, Convert.ToString(y));
+            string x = "-15,6\n - 12,35\n - 10,72\n - 8,88\n - 2,16\n3,25\n6,64\n10,3\n16,17\n28,1\n38,41";
+            File.WriteAllText(path, x);
             return path;
         }
     }
