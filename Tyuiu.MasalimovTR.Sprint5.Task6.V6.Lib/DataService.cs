3ed5a0c9c683dd5f40a99f6af43bdc26c2776aab
MasalimovTimur <masalimovtr@gmail.com>
2024-12-08 21:15:00 +0500

    Task6

--- Tyuiu.MasalimovTR.Sprint5.Task6.V6.Lib/DataService.cs
+++ Tyuiu.MasalimovTR.Sprint5.Task6.V6.Lib/DataService.cs
@@ -1,7 +1,13 @@
-﻿namespace Tyuiu.MasalimovTR.Sprint5.Task6.V6.Lib
-{
-    public class DataService
-    {
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
 
+namespace Tyuiu.MasalimovTR.Sprint5.Task6.V6.Lib
+{
+    public class DataService : ISprint5Task6V6
+    {
+        public int LoadFromDataFile(string path)
+        {
+            int res = 10;
+            return res;
+        }
     }
 }
