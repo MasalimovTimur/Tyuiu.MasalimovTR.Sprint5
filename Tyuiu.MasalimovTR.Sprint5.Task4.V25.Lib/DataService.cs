1a77a68dc39e77d8b62352cc9e40052c3f0660f1
MasalimovTimur <masalimovtr@gmail.com>
2024-12-08 18:42:17 +0500

    Task3

--- Tyuiu.MasalimovTR.Sprint5.Task4.V25.Lib/DataService.cs
+++ Tyuiu.MasalimovTR.Sprint5.Task4.V25.Lib/DataService.cs
@@ -1,7 +1,15 @@
-﻿namespace Tyuiu.MasalimovTR.Sprint5.Task4.V25.Lib
-{
-    public class DataService
-    {
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
 
+namespace Tyuiu.MasalimovTR.Sprint5.Task4.V25.Lib
+{
+    public class DataService : ISprint5Task4V25
+    {
+        public double LoadFromDataFile(string path)
+        {
+            string strx = File.ReadAllText(path);
+
+            double res = Math.Round(Convert.ToDouble(strx),3);
+            return res;
+        }
     }
 }
