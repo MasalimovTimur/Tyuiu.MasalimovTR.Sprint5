1a92abccc9b6038d552b05244fb4f002700015d7
MasalimovTimur <masalimovtr@gmail.com>
2024-12-14 01:55:02 +0500

    Task7

--- Tyuiu.MasalimovTR.Sprint5.Task7.V25.Lib/DataService.cs
+++ Tyuiu.MasalimovTR.Sprint5.Task7.V25.Lib/DataService.cs
@@ -1,7 +1,12 @@
-﻿namespace Tyuiu.MasalimovTR.Sprint5.Task7.V25.Lib
-{
-    public class DataService
-    {
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
 
+namespace Tyuiu.MasalimovTR.Sprint5.Task7.V25.Lib
+{
+    public class DataService : ISprint5Task7V25
+    {
+        public string LoadDataAndSave(string path)
+        {
+            return "123";
+        }
     }
 }
